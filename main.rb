bcf1df3f40dd22cb52d556b1185d220b92390b9d
bofmar <bofmar@outlook.com>
2020-10-14 18:32:51 +0300

    added player class

--- main.rb
+++ main.rb
@@ -1,5 +1,13 @@
 # Used for testing for now
 
 require_relative 'grid'
+require_relative 'player'
 
-grid = Grid.new
+#grid = Grid.new
+player = Player.new "Mario", "M"
+
+player.acquire_tile "1"
+p player.name
+p player.owned_tiles
+p player.symbol
+
---
+++ player.rb
@@ -0,0 +1,27 @@
+class Player
+  attr_reader :owned_tiles
+  attr_reader :name
+  attr_reader :symbol
+
+  def initialize name, symbol
+    @owned_tiles = []
+    @name = name
+    if symbol.is_number?
+      @symbol = 'X'
+    elsif symbol.length == 1
+      @symbol = symbol
+    else
+      @symbol = 'X'
+    end
+  end
+
+  def acquire_tile tile
+    @owned_tiles << tile
+  end
+end
+
+class Object
+  def is_number?
+    to_f.to_s == to_s || to_i.to_s == to_s
+  end
+end
